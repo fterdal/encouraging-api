29e7c4d7b2dee3da8ca7b6c9c5b37e918109be32
Finn Terdal <fterdal@gmail.com>
2019-03-04 16:50:19 -0600

    change package json

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const app = express()
 
 app.use(volleyball)
 
-const encouragingGifs = [
-  { url: 'https://media.giphy.com/media/12vJgj7zMN3jPy/giphy.gif' },
-  { url: 'https://media.giphy.com/media/LnaPDokBEmONG/giphy.gif' },
-  { url: 'https://media.giphy.com/media/247EDLfwn3tPLC1tNB/giphy.gif' },
-  { url: 'https://media.giphy.com/media/F3lPUjF1bb4go/giphy.gif' },
-  { url: 'https://media.giphy.com/media/xN6R8Nis6Iz3W/giphy.gif' },
-  { url: 'https://media.giphy.com/media/mbhseRYedlG5W/giphy.gif' },
-  { url: 'https://media.giphy.com/media/JVdF14CQQH7gs/giphy.gif' },
-  { url: 'https://media.giphy.com/media/26ufhqlbeuJsUbvDG/giphy.gif' },
-  { url: 'https://media.giphy.com/media/lcYFNTaz4U9jy/giphy.gif' },
-]
+// const encouragingGifs = [
+//   { url: 'https://media.giphy.com/media/12vJgj7zMN3jPy/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/LnaPDokBEmONG/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/247EDLfwn3tPLC1tNB/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/F3lPUjF1bb4go/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/xN6R8Nis6Iz3W/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/mbhseRYedlG5W/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/JVdF14CQQH7gs/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/26ufhqlbeuJsUbvDG/giphy.gif' },
+//   { url: 'https://media.giphy.com/media/lcYFNTaz4U9jy/giphy.gif' },
+// ]
 
-const pickRandom = (gifs) => {
-  return gifs[Math.floor(Math.random() * gifs.length)]
-}
+// const pickRandom = (gifs) => {
+//   return gifs[Math.floor(Math.random() * gifs.length)]
+// }
 
 app.get('/', (req, res) => {
   res.send('HELLO FROM HEROKU!')

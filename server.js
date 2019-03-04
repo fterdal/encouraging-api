5f72d95e7f887516c2a5a19bcd0a8cf3be42261d
Finn Terdal <fterdal@gmail.com>
2019-03-04 16:31:20 -0600

    now with a link

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const encouragingGifs = [
   { url: 'https://media.giphy.com/media/LnaPDokBEmONG/giphy.gif' },
   { url: 'https://media.giphy.com/media/247EDLfwn3tPLC1tNB/giphy.gif' },
   { url: 'https://media.giphy.com/media/F3lPUjF1bb4go/giphy.gif' },
+  { url: 'https://media.giphy.com/media/xN6R8Nis6Iz3W/giphy.gif' },
+  { url: 'https://media.giphy.com/media/mbhseRYedlG5W/giphy.gif' },
+  { url: 'https://media.giphy.com/media/JVdF14CQQH7gs/giphy.gif' },
+  { url: 'https://media.giphy.com/media/26ufhqlbeuJsUbvDG/giphy.gif' },
+  { url: 'https://media.giphy.com/media/lcYFNTaz4U9jy/giphy.gif' },
 ]
 
 const pickRandom = (gifs) => {
@@ -17,7 +22,12 @@ const pickRandom = (gifs) => {
 
 app.use((req, res, next) => {
   res.send(`
+  <div>
     <img src="${pickRandom(encouragingGifs).url}"></img>
+    <p>Check out the source code
+      <a href="https://github.com/fterdal/encouraging-api">here</a>
+    </p>
+  </div>
   `)
 })
 

ece1bf7e16df3e9b4e5c163c0ef42b75d9e55d62
Finn Terdal <fterdal@gmail.com>
2019-03-04 17:06:27 -0600

    aha, it seems it was volleyball that broke

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
 const express = require('express')
-// const volleyball = require('volleyball')
+const morgan = require('morgan')
 const app = express()
 
-// app.use(volleyball)
+app.use(morgan('dev'))
 
-// const encouragingGifs = [
-//   { url: 'https://media.giphy.com/media/12vJgj7zMN3jPy/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/LnaPDokBEmONG/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/247EDLfwn3tPLC1tNB/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/F3lPUjF1bb4go/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/xN6R8Nis6Iz3W/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/mbhseRYedlG5W/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/JVdF14CQQH7gs/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/26ufhqlbeuJsUbvDG/giphy.gif' },
-//   { url: 'https://media.giphy.com/media/lcYFNTaz4U9jy/giphy.gif' },
-// ]
+const encouragingGifs = [
+  { url: 'https://media.giphy.com/media/12vJgj7zMN3jPy/giphy.gif' },
+  { url: 'https://media.giphy.com/media/LnaPDokBEmONG/giphy.gif' },
+  { url: 'https://media.giphy.com/media/247EDLfwn3tPLC1tNB/giphy.gif' },
+  { url: 'https://media.giphy.com/media/F3lPUjF1bb4go/giphy.gif' },
+  { url: 'https://media.giphy.com/media/xN6R8Nis6Iz3W/giphy.gif' },
+  { url: 'https://media.giphy.com/media/mbhseRYedlG5W/giphy.gif' },
+  { url: 'https://media.giphy.com/media/JVdF14CQQH7gs/giphy.gif' },
+  { url: 'https://media.giphy.com/media/26ufhqlbeuJsUbvDG/giphy.gif' },
+  { url: 'https://media.giphy.com/media/lcYFNTaz4U9jy/giphy.gif' },
+]
 
-// const pickRandom = (gifs) => {
-//   return gifs[Math.floor(Math.random() * gifs.length)]
-// }
+const pickRandom = (gifs) => {
+  return gifs[Math.floor(Math.random() * gifs.length)]
+}
 
-app.get('/', (req, res) => {
-  res.send('HELLO FROM HEROKU!')
-})
-
-// app.get('/', (req, res, next) => {
-//   res.send(`
-//   <div>
-//     <img src="${pickRandom(encouragingGifs).url}"></img>
-//     <p>Check out the source code
-//       <a href="https://github.com/fterdal/encouraging-api">here</a>
-//     </p>
-//   </div>
-//   `)
+// app.get('/', (req, res) => {
+//   res.send('HELLO FROM HEROKU!')
 // })
+
+app.get('/', (req, res, next) => {
+  res.send(`
+  <div>
+    <img src="${pickRandom(encouragingGifs).url}"></img>
+    <p>Check out the source code
+      <a href="https://github.com/fterdal/encouraging-api">here</a>
+    </p>
+  </div>
+  `)
+})
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
